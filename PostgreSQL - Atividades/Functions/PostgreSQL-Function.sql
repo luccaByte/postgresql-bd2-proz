ec9a774c75c6ad46cd7e82eb0118a87bf1e9f9e0
lucas <lucaston2323@gmail.com>
2023-12-23 00:02:36 -0300

    feat: function

---
+++ PostgreSQL - Atividades/Functions/PostgreSQL-Function.sql
@@ -0,0 +1,31 @@
+-- Criando a tabela
+CREATE TABLE clientes (
+    id_cliente SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    data_cadastro TIMESTAMP
+);
+
+INSERT INTO clientes (nome, email) VALUES
+    ('João Silva', 'joaosilva@gmail.com', '2023-12-21'),
+    ('Maria Oliveira', 'mariaoliveira@gmail.com', '2021-01-10'),
+    ('Carlos Santos', 'carlossantos@gmail.com', '2019-07-05');
+
+
+-- Criação da função
+
+CREATE OR REPLACE FUNCTION contar_clientes_cadastrados(data_consulta DATE)
+RETURNS INTEGER AS
+$$
+DECLARE
+    total_clientes INTEGER;
+BEGIN@
+    SELECT COUNT(*) INTO total_clientes
+    FROM tabela_clientes
+    WHERE data_cadastro::DATE = data_consulta;
+
+    RETURN total_clientes;
+END;
+$$
+LANGUAGE plpgsql;
+SELECT contar_clientes_cadastrados('2023-12-22') AS total_clientes_cadastrados;
---
+++ PostgreSQL - Atividades/Functions/readme.md
@@ -0,0 +1,3 @@
+# Instruções do projeto
+
+Uma loja tem um banco de dados que contém todo o controle de vendas de produtos e de cadastro de clientes. Pensando nisso, crie uma função para somar todos os clientes que foram cadastrados na loja durante um dia.
